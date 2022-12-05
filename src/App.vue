c860154a6524cd13d74f6cd86da31517b9052795
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-12-04 20:56:48 -0600

    init commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div style="height: 100vh; width: 100vw">
+        <baklava-editor :view-model="baklava" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { EditorComponent, useBaklava } from "@baklavajs/renderer-vue";
+import CANNode from "./node/CANNode";
+
+export default defineComponent({
+    components: {
+        "baklava-editor": EditorComponent,
+    },
+    setup() {
+        const baklava = useBaklava();
+        console.log(baklava);
+        baklava.editor.registerNodeType(CANNode);
+        return { baklava };
+    },
+});
+</script>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+// import "./assets/main.css";
+
+import "@baklavajs/themes/dist/syrup-dark.css";
+
+createApp(App).mount("#app");
